e03b306b46d897c72db3473b36684c7a27f7a751
235asf <3205271587@qq.com>
2026-08-26 19:28:16 +0800

    implement syscall attack

--- user/attack.c
+++ user/attack.c
@@ -3,10 +3,28 @@
 #include "user/user.h"
 #include "kernel/riscv.h"
 
+#define PGSIZE 4096
 int
 main(int argc, char *argv[])
 {
   // Your code here.
+  int npages = 64;
+  char *mem = sbrk(npages * PGSIZE);
+  if (mem == (char *)-1) {
+    printf("attack: sbrk failed\n");
+    exit(1);
+  }
 
+  char *pattern = "help.";
+  int pat_len = strlen(pattern);
+  int total_len = npages * PGSIZE;
+
+  for (int i = 0; i < total_len - 32; i++) {
+    if (memcmp(mem + i, pattern, pat_len) == 0) {
+      char *secret = mem + i + 7;
+      printf("%s\n", secret);
+      exit(0);
+    }
+  }
   exit(1);
 }
